f5abcfe15b64b497d4f093a25b79e3a5836e07ea
Ana Piedrahita <ana_piedrahita@epam.com>
2022-05-31 23:25:15 -0500

    added confirmation

--- fundamentals_2/a_simple_page.js
+++ fundamentals_2/a_simple_page.js
@@ -1,3 +1,5 @@
 let currentUserName = prompt('What is your name?');
+let currentUserConfirmation = confirm(`Are you sure that your name is ${currentUserName}?`);
 
-alert(`Your name is ${currentUserName}`);
+alert(currentUserConfirmation);
+alert(`Thank you for confirming, ${currentUserName}`);
