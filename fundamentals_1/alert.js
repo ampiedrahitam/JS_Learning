086f99b7bb8e9b59f03323143e6dbbd08890c5a6
Ana Piedrahita <ana_piedrahita@epam.com>
2022-05-31 22:51:22 -0500

    changes in the directory

---
+++ fundamentals_1/alert.js
@@ -0,0 +1,10 @@
+let name = 'John';
+let admin = name;
+let planetName = 'Earth';
+let currenVisitorName
+
+
+alert(admin);
+alert( `hello ${1}` ); // hello 1
+alert( `hello ${"name"}` ); // hello name
+alert( `hello ${name}` ); // hello John
