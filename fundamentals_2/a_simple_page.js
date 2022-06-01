fb7276e67a95202b2c751c0e70f56918edc8de75
Ana Piedrahita <ana_piedrahita@epam.com>
2022-05-31 23:32:05 -0500

    added ifelse for confrimation

--- fundamentals_2/a_simple_page.js
+++ fundamentals_2/a_simple_page.js
@@ -2,4 +2,8 @@ let currentUserName = prompt('What is your name?');
 let currentUserConfirmation = confirm(`Are you sure that your name is ${currentUserName}?`);
 
 alert(currentUserConfirmation);
-alert(`Thank you for confirming, ${currentUserName}`);
+if (currentUserConfirmation==false) {
+    alert('Thank you for your honesty');
+} else {
+    alert(`Thank you for confirming, ${currentUserName}`);
+}
