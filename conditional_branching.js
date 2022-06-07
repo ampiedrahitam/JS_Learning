2394bfe60013ec5bdae4c3807b43a66a438e3671
Ana Piedrahita <ana_piedrahita@epam.com>
2022-06-06 20:17:11 -0500

    changed login value

--- conditional_branching.js
+++ conditional_branching.js
@@ -22,7 +22,7 @@ let result = (a + b < 4) ? 'Below': 'Over';
 
 alert(result);
 
-let login = 'Director'
+let login = 'Employee'
 let message = (login == 'Employee') ? 'Hello':
     (login == 'Director') ? 'Greetings':
     (login == '') ? 'No login':
