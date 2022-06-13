172bfe50d9e24ec4df6434c5a4e852204857ab57
Ana Piedrahita <ana_piedrahita@epam.com>
2022-06-13 18:49:19 -0500

    added more exercises to logical operators

--- logical_operators.js
+++ logical_operators.js
@@ -1,16 +1,16 @@
-let currentUserAge = prompt ('What is your age?', '');
+/*let currentUserAge = prompt ('What is your age?', '');
 
-/*if (currentUserAge >= 14 && currentUserAge <= 90) {
+if (currentUserAge >= 14 && currentUserAge <= 90) {
     alert('Nailed It!')
 } else {
     alert("I've got it wrong!")
-};*/
+};
 
-/*if (currentUserAge = ! (currentUserAge >= 14 && currentUserAge <= 90)) {
+if (currentUserAge = ! (currentUserAge >= 14 && currentUserAge <= 90)) {
     alert('Nailed It!')
 } else {
     alert("I've got it wrong!")
-};*/
+};
 
 if (currentUserAge < 14 || currentUserAge > 90) {
     alert('Nailed It!')
@@ -19,8 +19,31 @@ if (currentUserAge < 14 || currentUserAge > 90) {
 };
 
 
-/*alert( null || 2 || undefined ); // 2 correct
+alert( null || 2 || undefined ); // 2 correct
 alert( alert(1) || 2 || alert(3) ); // 1 incorrect 1 then 2
 alert( 1 && null && 2 ); // null correct
 alert( alert(1) && alert(2) ); // 2 incorrect 1 then undefined
-alert( null || 2 && 3 || 4 ); // 3 correct*/
+alert( null || 2 && 3 || 4 ); // 3 correct
+
+if (-1 || 0) alert( 'first' ); // executed
+if (-1 && 0) alert( 'second' ); // not executed
+if (null || -1 && 1) alert( 'third' ); //executed */
+
+let currentUserName = prompt('Who is there?', '');
+
+if (currentUserName == null ) {
+    alert('Canceled')
+} else if (currentUserName != 'Admin') {
+    alert('I do not know you!')
+} else {
+    if (currentUserName == 'Admin') {
+        currentUserPassword = prompt('Password?')
+        if (currentUserName == 'Admin' && currentUserPassword == null) {
+            alert('Canceled')
+        } else if (currentUserName == 'Admin' && currentUserPassword != 'TheMaster'){
+            alert('Wrong Password')
+        }else {
+            alert('Welcome!')
+        }
+    }
+};
